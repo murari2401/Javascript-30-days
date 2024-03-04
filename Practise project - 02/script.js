a042fed6149e4dcf7a6f0a58ad35e386104ad81c
murari2401 <iammurari89@gmail.com>
2024-03-05 01:03:34 +0530

    Added throttle function

--- Practise project - 02/script.js
+++ Practise project - 02/script.js
@@ -1,3 +1,18 @@
-document.querySelector("#center").addEventListener("mousemove",function(dets){
-    console.log(dets.clientX,dets.clientY);
-});
+// document.querySelector("#center").addEventListener("mousemove",function(dets){
+//     console.log(dets.clientX,dets.clientY);
+// });
+
+// const btn = document.querySelector("#throttle");
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
+    }
+}
+document.querySelector("#center").addEventListener("mousemove",throttleFunction(() => {
+        console.log("Moved")
+    }, 1500));

f7f0ba1b40b28cfa097940871005bb586bed3361
murari2401 <iammurari89@gmail.com>
2024-03-17 19:12:36 +0530

    final

--- Practise project - 02/script.js
+++ Practise project - 02/script.js
@@ -2,7 +2,6 @@
 //     console.log(dets.clientX,dets.clientY);
 // });
 
-// const btn = document.querySelector("#throttle");
 const throttleFunction = (func, delay) => {
     let prev = 0;
     return (...args) => {
@@ -13,6 +12,15 @@ const throttleFunction = (func, delay) => {
         }
     }
 }
-document.querySelector("#center").addEventListener("mousemove",throttleFunction(() => {
-        console.log("Moved")
-    }, 1500));
+document.querySelector("#center").addEventListener("mousemove", throttleFunction((dets) => {
+    var div = document.createElement("div");
+    div.classList.add("imagediv");
+    div.style.left = dets.clientX + 'px';
+    div.style.top = dets.clientY + 'px';
+    var img=document.createElement("img");
+    img.setAttribute("src" , "")
+    document.body.appendChild(div);
+    setTimeout(function(){
+        div.remove();
+    },2000);
+}, 400));
